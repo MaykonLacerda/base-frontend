b497161fb1ac0e5402606799f654ad154ab41dc3
Maykon Lacerda <maykon@smartinnovation.com.br>
2022-08-29 19:35:28 -0300

    feat: setup redux toolkit query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './state/store';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppWrapper />);
